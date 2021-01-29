f36e2007c76831255f05bacd95015f5c778fb183
smoort <sarav_m@hotmail.com>
2021-01-29 11:35:10 +0530

    updated

--- nn.py
+++ nn.py
@@ -1,25 +1,37 @@
 """
-Have fun with the number of epochs!
-
-Be warned that if you increase them too much,
-the VM will time out :)
+My own neural net implementation
+Courtesy Miniflow from Udacity
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.datasets import load_boston
 from sklearn.utils import shuffle, resample
 from miniflow import *
+import pandas as pd
 
 # Load data
 data = load_boston()
-X_ = data['data']
-y_ = data['target']
 
-# Normalize data
+print("data type", type(data))
+
+X_ = data['data']
+print("X_ type", type(X_))
+print("X_ shape", X_.shape)
+print("X_ size", X_.size)
+#print(X_[1])
+y_ = data['target']
+print("y_ type", type(y_))
+print("y_ shape", y_.shape)
+print("y_ size", y_.size)
+print(y_[1])
+
+# Standardize data
 X_ = (X_ - np.mean(X_, axis=0)) / np.std(X_, axis=0)
 
 n_features = X_.shape[1]
-n_hidden = 10
+n_hidden = 3
 W1_ = np.random.randn(n_features, n_hidden)
 b1_ = np.zeros(n_hidden)
 W2_ = np.random.randn(n_hidden, 1)
@@ -44,7 +56,7 @@ feed_dict = {
     b2: b2_
 }
 
-epochs = 1000
+epochs = 10
 # Total number of examples
 m = X_.shape[0]
 batch_size = 11
@@ -54,6 +66,9 @@ graph = topological_sort(feed_dict)
 trainables = [W1, b1, W2, b2]
 
 print("Total number of examples = {}".format(m))
+
+loss_list = []
+loss_drop_list = [0]
 
 # Step 4
 for i in range(epochs):
@@ -76,3 +91,13 @@ for i in range(epochs):
         loss += graph[-1].value
 
     print("Epoch: {}, Loss: {:.3f}".format(i+1, loss/steps_per_epoch))
+    if i > 0:
+        loss_drop_list.append(loss_list[-1] - (loss/steps_per_epoch))
+    loss_list.append(loss/steps_per_epoch)    
+
+loss_drop_list[0] = loss_drop_list[1]
+df = pd.DataFrame(data={"Loss": loss_list, "Loss Drop": loss_drop_list})
+print(loss_list)
+print(loss_drop_list)
+sns.lineplot(data=df)
+plt.show()
